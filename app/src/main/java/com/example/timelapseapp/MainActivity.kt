5651d51c49db05a551168691b74b6180cb591143
supin <qorgkr26@sookmyung.ac.kr>
2021-09-28 17:35:46 +0900

    feat : 간단한 메인 화면 구현

--- app/src/main/java/com/example/timelapseapp/MainActivity.kt
+++ app/src/main/java/com/example/timelapseapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.timelapseapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cameraButton = findViewById<Button>(R.id.takeTimelapseBtn)
+        val editButton = findViewById<Button>(R.id.editTimelapseBtn)
+
+        clickButton(cameraButton)
+        clickButton(editButton)
+    }
+
+    fun clickButton(button: Button) {
+        button.setOnClickListener(View.OnClickListener {
+            var intent = when(it?.id) {
+                R.id.takeTimelapseBtn -> Intent(this,CameraActivity::class.java)
+                R.id.editTimelapseBtn -> Intent(this,EditActivity::class.java)
+                else -> null
+            }
+            intent?.let { startActivity(intent) }
+        })
     }
 }
